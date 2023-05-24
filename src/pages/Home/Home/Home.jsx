7baedbb46aaa04c0963e5130ba29c0cd265f1df6
mobassherkhandakar <118150012+mobassherkhandakar@users.noreply.github.com>
2023-05-25 01:44:34 +0600

    make apoinment done

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import InfoCards from '../infoCards/infoCards';
+import Servises from '../Servises/Servises';
+import MakeAppoinment from '../MakeAppoinment/MakeAppoinment';
 
 const Home = () => {
   return (
     <div>
       <Banner/>
+      <InfoCards/>
+      <Servises/>
+      <MakeAppoinment/>
     </div>
   );
 };
